58e1d0ad46a3cabb442f288f2ca0598b601f340d
JO WONYOUNG <whdnjsdud551@gmail.com>
2024-07-26 21:58:33 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import History from "./pages/History/History";
+import Post from "./pages/Post/Post";
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/history/*" element={<History />} />
+        <Route path="/post/*" element={<Post />} />
+        <Route path="/register/*" element={<Register />} />
+        <Route path="/login/*" element={<Login />} />
+        <Route path="signup/*" element={<Signup />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
